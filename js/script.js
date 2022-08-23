5360fc1dad734e3098655541f8fd8e615776b329
lorena bastos <bastosoliveira6@gmail.com>
2022-08-22 23:03:48 -0300

    Atualizando atividade 1 de JS
    Enviando segunda parte da atividade de JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-{
-    let recebePrimeiroNumero = 0;
-    let recebeSegundoNumero = 0;
 
-    recebePrimeiroNumero = 10;
-    recebeSegundoNumero = 5;
-    console.log(recebePrimeiroNumero + recebeSegundoNumero)
-}
+ 
+
+ const mensagem = "Parabéns, você multiplicou 2 números!";
+ let recebePrimeiroNumero = 0; 
+ let recebeSegundoNumero = 0; 
+ recebePrimeiroNumero = prompt('Digite o primeiro número');
+ recebeSegundoNumero = prompt('Digite o segundo número');
+ console.log(mensagem);
+ console.log(parseInt(recebePrimeiroNumero)*parseInt(recebeSegundoNumero));
