62f15e6257cbf3b94b738d2e20a811691d8fc581
lorena bastos <bastosoliveira6@gmail.com>
2022-08-22 22:31:13 -0300

    Primeira atividade de JS
    Enviando primeira parte da atividade de prática de JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+{
+    let recebePrimeiroNumero = 0;
+    let recebeSegundoNumero = 0;
+
+    recebePrimeiroNumero = 10;
+    recebeSegundoNumero = 5;
+    console.log(recebePrimeiroNumero + recebeSegundoNumero)
+}
