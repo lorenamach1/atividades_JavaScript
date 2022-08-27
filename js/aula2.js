175b7af2854360953e9527647108d46aeeabcb67
lorena bastos <bastosoliveira6@gmail.com>
2022-08-27 15:23:25 -0300

    Atividade aula 2 js

---
+++ js/aula2.js
@@ -0,0 +1,21 @@
+//ATIVIDADE JS
+
+// QUESTÃO A
+let adesivos = (7*30)-15;
+console.log(adesivos);
+
+// QUESTÃO B
+let caixa = (2*12)*4;
+console.log(caixa);
+
+// QUESTÃO C 
+let numeroDecremento = 9;
+console.log(numeroDecremento--);
+numeroDecremento-= 1;
+console.log(numeroDecremento--);
+numeroDecremento-= 1;
+console.log(numeroDecremento--);
+numeroDecremento-= 1;
+console.log(numeroDecremento--);
+numeroDecremento-= 1;
+console.log(numeroDecremento--);
